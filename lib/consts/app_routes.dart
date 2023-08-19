a5c6bf126ef6e561f015b00ee63386241a59b011
Elvis <elvis@ezsolucoes.com.br>
2023-08-19 19:50:53 -0300

    feature(AppRoutes): adiciona a rota appQuestionnairePage.

--- lib/consts/app_routes.dart
+++ lib/consts/app_routes.dart
@@ -3,5 +3,6 @@ class AppRoutes{
   static const String appDefaultPage = '/';
   static const String appSplashPage = '/splash';
   static const String appHomePage = '/home';
+  static const String appQuestionnairePage= '/Questionnaire';
 
 }

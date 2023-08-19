9f8040a326bff910f3c97a8ddff7fe955e0ac051
Elvis <elvis@ezsolucoes.com.br>
2023-08-19 14:50:33 -0300

    feature(AppDimens): cria a constante de dimensoes.

---
+++ lib/consts/app_dimens.dart
@@ -0,0 +1,23 @@
+class AppDimens {
+  static const double zeroDimension = 0.0;
+
+  //Margin
+  static const double tinyMarginDimension = 4.0;
+  static const double defaultMarginDimension = 16.0;
+  static const double littleBigMarginDimension = 20.0;
+  static const double veryBigMarginDimension = 32.0;
+  static const double ampleMarginDimension = 48.0;
+  static const double veryLargeMarginDimension = 64.0;
+
+  //fonts
+  static const double fontSizeTinyDimension = 10;
+  static const double fontSizeMiniDimension = 12;
+  static const double fontSizeSmallDimension = 14;
+  static const double fontSizeMediumDimension = 16;
+  static const double fontSizeBigDimension = 18;
+  static const double fontSizeLargeDimension = 20;
+  static const double fontSizeHugeDimension = 22;
+  static const double fontSizeGiantDimension = 24;
+
+
+}

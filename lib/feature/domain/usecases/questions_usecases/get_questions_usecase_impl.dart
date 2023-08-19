c3d8a5e93310be97760539263dc1a9a5aebca346
Elvis <elvis@ezsolucoes.com.br>
2023-08-19 16:31:33 -0300

    feature(GetQuestionsUsecase): cria a implementacao da abstracao do usecase.

---
+++ lib/feature/domain/usecases/questions_usecases/get_questions_usecase_impl.dart
@@ -0,0 +1,22 @@
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/usecase/usecase.dart';
+import '../../../../core/errors/application_error.dart';
+
+import '../../../../feature/domain/repositories/questions_repositories/questions_repositories.dart';
+import '../../../../feature/domain/entities/questions_entities/questions_entity.dart';
+
+import 'get_questions_usecase.dart';
+
+class GetQuestionsUsecaseImpl implements GetQuestionsUsecase{
+
+  final QuestionsRepositories repository;
+
+  const GetQuestionsUsecaseImpl(this.repository);
+
+  @override
+  Future<Either<ApplicationError, QuestionsEntity>> call(NoParams noParams) async {
+    return await repository.getQuestions(noParams);
+  }
+
+}

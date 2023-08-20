42a4f84414368131df3051c210c6c4f32208498a
Elvis <elvis@ezsolucoes.com.br>
2023-08-19 21:02:42 -0300

    feature(QuestionAnswerEntity): cria a classe.

---
+++ lib/feature/domain/entities/questionnaires_entities/question_answer_entity.dart
@@ -0,0 +1,11 @@
+import 'package:fast_trivia/feature/domain/entities/questionnaires_entities/questionnaire_entity.dart';
+
+class QuestionAnswerEntity {
+
+  final QuestionnaireEntity questionnaire;
+
+  QuestionAnswerEntity({
+    required this.questionnaire,
+  });
+
+}

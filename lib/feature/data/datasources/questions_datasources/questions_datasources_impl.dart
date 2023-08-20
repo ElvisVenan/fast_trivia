5bf80d6f6ed11fa25da739f8260d2eb153c47a97
Elvis <elvis@ezsolucoes.com.br>
2023-08-19 21:07:39 -0300

    refactor(QuestionsEntity): altera a entidade QuestionsEntity para QuestionAnswerModel.

--- lib/feature/data/datasources/questions_datasources/questions_datasources_impl.dart
+++ lib/feature/data/datasources/questions_datasources/questions_datasources_impl.dart
@@ -1,18 +1,17 @@
+import 'package:fast_trivia/feature/data/models/questionnaire_models/question_answer_model.dart';
 import 'package:flutter/services.dart';
 
 import '../../../../core/usecase/usecase.dart';
 
 import '../../../../feature/data/datasources/questions_datasources/questions_datasources.dart';
-import '../../../../feature/domain/entities/questions_entities/questions_entity.dart';
-import '../../../../feature/data/models/questions_models/questions_model.dart';
 
 class QuestionsDatasourcesImpl implements QuestionsDataSources{
   @override
-  Future<QuestionsEntity> getQuestions(NoParams noParams) async {
+  Future<QuestionAnswerModel> getQuestions(NoParams noParams) async {
 
-    final jsonString = await rootBundle.loadString('assets/jsons/teste.json');
+    final jsonString = await rootBundle.loadString('assets/jsons/questions.json');
 
-    final post = QuestionsModel.fromJson(jsonString);
+    final post = QuestionAnswerModel.fromJson(jsonString);
 
     return post;
 

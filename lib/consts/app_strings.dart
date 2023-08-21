c6571c786dd7284c062c996f1c329565d2ca0471
Elvis <elvis@ezsolucoes.com.br>
2023-08-21 13:11:34 -0300

    feature(AppStrings): adiciona as strings resultString, correctString, noHits e answers.

--- lib/consts/app_strings.dart
+++ lib/consts/app_strings.dart
@@ -5,5 +5,9 @@ class AppStrings{
   static const String newQuestionnaireString = 'Novo Questionário';
   static const String questionnairesString = 'Questionários ';
   static const String confirmString = 'Confirmar ';
+  static const String resultString = 'Resultado ';
+  static const String correctString = 'acertos ';
+  static const String noHits = 'não houve';
+  static const String answers = 'Respostas';
 
 }

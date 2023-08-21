ea834f038e2c6f6d7e4e2cc79a4b20c030861661
Elvis <elvis@ezsolucoes.com.br>
2023-08-21 11:09:57 -0300

    feature(AppModule): cria as rotas appCompletedQuestionnairePage e appHistoricalPage.

--- lib/consts/app_routes.dart
+++ lib/consts/app_routes.dart
@@ -4,5 +4,7 @@ class AppRoutes{
   static const String appSplashPage = '/splash';
   static const String appHomePage = '/home';
   static const String appQuestionnairePage= '/Questionnaire';
+  static const String appCompletedQuestionnairePage = '/CompletedQuestionnaire';
+  static const String appHistoricalPage = '/HistoricalPage';
 
 }

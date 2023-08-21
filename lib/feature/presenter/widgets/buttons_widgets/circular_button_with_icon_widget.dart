4adf7702e49fe51ab293c89b826b1b5fff7077c4
Elvis <elvis@ezsolucoes.com.br>
2023-08-21 16:07:45 -0300

    feature(CircularButtonWithIconWidget): cria a classe.

---
+++ lib/feature/presenter/widgets/buttons_widgets/circular_button_with_icon_widget.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import '../../../../consts/app_colors.dart';
+import '../../../../consts/app_dimens.dart';
+
+class CircularButtonWithIconWidget extends StatelessWidget {
+  final void Function()? onPressed;
+  final IconData? icon;
+
+  const CircularButtonWithIconWidget({super.key, this.onPressed, this.icon});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 50,
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          shape: const CircleBorder(),
+          padding: const EdgeInsets.all(AppDimens.defaultMarginDimension),
+        ),
+        child: Transform.scale(
+          scale: 1.2,
+          child: Icon(
+            icon,
+            color: AppColors.pureWhiteColor,
+            size: AppDimens.bigMarginDimension,
+          ),
+        ),
+      ),
+    );
+  }
+}

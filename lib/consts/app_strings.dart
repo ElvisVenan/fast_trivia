aa406893a13026d317d2a28e5077929b95e21a76
Elvis <elvis@ezsolucoes.com.br>
2023-08-21 11:21:02 -0300

    refactor(HomePage): ajusta a home page para que fique legivel.

--- lib/consts/app_strings.dart
+++ lib/consts/app_strings.dart
@@ -3,5 +3,6 @@ class AppStrings{
   static const String nameOfTheApplication = 'Fast Trivia';
 
   static const String newQuestionnaire = 'Novo Questionário';
+  static const String questionnaires = 'Questionários ';
 
 }
--- lib/feature/presenter/ui/home_page/home_page.dart
+++ lib/feature/presenter/ui/home_page/home_page.dart
@@ -1,33 +1,61 @@
-import 'package:fast_trivia/feature/presenter/controllers/questions_controller/questions_controller.dart';
+import 'package:fast_trivia/feature/presenter/ui/questionnaire_page/questionnaire_page.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 import '../../../../consts/app_colors.dart';
+import '../../../../consts/app_dimens.dart';
 import '../../../../consts/app_routes.dart';
 
+import '../../../../consts/app_strings.dart';
+import '../../../../feature/domain/params/args_params/args_params.dart';
+import '../../../../feature/presenter/controllers/questions_controller/questions_controller.dart';
+import '../../widgets/buttons_widgets/rounded_edge_button_widget.dart';
+
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  final ArgsParams? args;
+
+  const HomePage({super.key, this.args});
 
   static const String routePath = AppRoutes.appHomePage;
 
-  static void navigate() => Modular.to.navigate(routePath);
+  static void navigate(ArgsParams args) =>
+      Modular.to.navigate(routePath, arguments: args);
+
+  static void push(ArgsParams args) =>
+      Modular.to.pushNamed(routePath, arguments: args);
 
   @override
   Widget build(BuildContext context) {
     final getQuestions = Modular.get<QuestionsController>();
     getQuestions.getQuestions();
-
     return Scaffold(
-      backgroundColor: AppColors.lightGray,
+      backgroundColor: AppColors.darkBlue,
       appBar: AppBar(
         backgroundColor: AppColors.darkBlue,
       ),
-      body: Center(
-        child: Observer(
-          builder: (context) {
-            return Text(getQuestions.question, style: TextStyle(color: Colors.black),);
-          }
+      body: Align(
+        alignment: Alignment.bottomCenter,
+        child: ClipRRect(
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(AppDimens.ampleMarginDimension),
+            topRight: Radius.circular(AppDimens.ampleMarginDimension),
+          ),
+          child: Container(
+            height: MediaQuery.sizeOf(context).height * 0.8,
+            width: double.infinity,
+            color: AppColors.lightGray,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                RoundedEdgeButtonWidget(
+                  text: AppStrings.newQuestionnaire,
+                  onPressed: () {
+                    QuestionnairePage.navigate(const ArgsParams());
+                  },
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );

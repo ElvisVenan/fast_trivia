be67b3b7710039ba83664e7953579ac001ccc51c
Elvis <elvis@ezsolucoes.com.br>
2023-08-22 11:07:36 -0300

    feature(HomePage): adiciona o historico.

--- lib/feature/presenter/ui/home_page/home_page.dart
+++ lib/feature/presenter/ui/home_page/home_page.dart
@@ -1,15 +1,19 @@
-import 'package:fast_trivia/feature/presenter/ui/questionnaire_page/questionnaire_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 import '../../../../consts/app_colors.dart';
 import '../../../../consts/app_dimens.dart';
 import '../../../../consts/app_routes.dart';
-
 import '../../../../consts/app_strings.dart';
+
+import '../../../../feature/presenter/controllers/questions_database_controller/questions_database_controller.dart';
+import '../../../../feature/presenter/ui/questionnaire_page/historical_page.dart';
+import '../../../../feature/presenter/ui/questionnaire_page/questionnaire_page.dart';
+import '../../../../feature/presenter/widgets/text_widgets/title_and_subtitle_widget.dart';
 import '../../../../feature/domain/params/args_params/args_params.dart';
-import '../../../../feature/presenter/controllers/questions_controller/questions_controller.dart';
 import '../../widgets/buttons_widgets/rounded_edge_button_widget.dart';
+import '../../widgets/containers_widgets/card_with_icon_widget.dart';
 
 class HomePage extends StatelessWidget {
   final ArgsParams? args;
@@ -26,8 +30,9 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final getQuestions = Modular.get<QuestionsController>();
-    getQuestions.getQuestions();
+    final getQuestionsController = Modular.get<QuestionsDatabaseController>();
+    getQuestionsController.getQuestionnairesFromDatabase();
+
     return Scaffold(
       backgroundColor: AppColors.darkBlue,
       appBar: AppBar(
@@ -44,17 +49,47 @@ class HomePage extends StatelessWidget {
             height: MediaQuery.sizeOf(context).height * 0.8,
             width: double.infinity,
             color: AppColors.pureWhiteColor,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                RoundedEdgeButtonWidget(
-                  text: AppStrings.newQuestionnaireString,
-                  onPressed: () {
-                    QuestionnairePage.navigate(const ArgsParams());
-                  },
-                ),
-              ],
-            ),
+            child: Observer(builder: (context) {
+              return Column(
+                children: [
+                  const TitleAndSubtitleWidget(
+                    title: AppStrings.historicalString,
+                    subtitle: AppStrings.seeQuestionsString,
+                  ),
+                  getQuestionsController.questionnaire!.isNotEmpty
+                      ? Align(
+                          alignment: Alignment.topCenter,
+                          child: SizedBox(
+                            height: MediaQuery.sizeOf(context).height * 0.3,
+                            width: MediaQuery.sizeOf(context).width * 0.9,
+                            child: ListView.builder(
+                                itemCount: 1,
+                                itemBuilder: (context, index) {
+                                  return
+                                    Observer(
+                                      builder: (context) {
+                                        return CardWithIconWidget(
+                                          onPressed: () => HistoricalPage.navigate(
+                                              const ArgsParams()),
+                                          title: getQuestionsController
+                                              .questionnaire?[0].title ??
+                                              "",
+                                        );
+                                      }
+                                    );
+                                }),
+                          ),
+                        )
+                      : const Text(AppStrings.noQuestionsString),
+                  RoundedEdgeButtonWidget(
+                    text: AppStrings.newQuestionnaireString,
+                    onPressed: () {
+                      QuestionnairePage.navigate(const ArgsParams());
+                    },
+                  ),
+                ],
+              );
+            }),
           ),
         ),
       ),

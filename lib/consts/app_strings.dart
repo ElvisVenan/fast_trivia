6272ff6e978b22c4268b3bd354a2bedbbda14989
Elvis <elvis@ezsolucoes.com.br>
2023-08-22 11:06:08 -0300

    feature(AppStrings): cria as strings seeQuestionsString e noQuestionsString.

--- lib/consts/app_strings.dart
+++ lib/consts/app_strings.dart
@@ -10,5 +10,7 @@ class AppStrings{
   static const String noHitsString = 'não houve';
   static const String answersString = 'Respostas';
   static const String historicalString = 'Histórico';
+  static const String seeQuestionsString = 'Veja as questões já respondidas';
+  static const String noQuestionsString = 'Não ha questões respondidas';
 
 }
